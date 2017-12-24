cbbbaaf28b28dc9295f0a0a1c61f6a929fbcbec5
Farid F. Mammadov <mr.farid.mammadov@gmail.com>
2017-12-24 17:07:03 +0400

    added method for getting received file

--- src/main/java/com/github/faridma/simplehttp/util/Extractor.java
+++ src/main/java/com/github/faridma/simplehttp/util/Extractor.java
@@ -1,13 +1,18 @@
 package com.github.faridma.simplehttp.util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
 
 /**
  *
  * @author Farid F. Mammadov
  */
 public class Extractor {
+
     public static String getStringValue(BufferedReader br) throws IOException {
         String inputLine;
         StringBuilder b = new StringBuilder();
@@ -15,5 +20,18 @@ public class Extractor {
             b.append(inputLine);
         }
         return b.toString();
+    }
+
+    /**
+     * This method designed to extract file from response and save it
+     * 
+     * @param c HTTP(S) connection
+     * @param f destination file, to save received file
+     * @throws IOException 
+     */
+    public static void saveFileFromOctetStream(HttpURLConnection c, File f) throws IOException {
+        try (InputStream is = c.getInputStream()) {
+            Files.copy(is, f.toPath());
+        }
     }
 }
