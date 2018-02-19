27c882eb51e0db054233d691b8310c41468b5941
Farid F. Mammadov <mr.farid.mammadov@gmail.com>
2018-02-19 15:18:59 +0400

    overload getConnection methods

--- src/main/java/com/github/faridma/simplehttp/core/HttpConnection.java
+++ src/main/java/com/github/faridma/simplehttp/core/HttpConnection.java
@@ -2,6 +2,7 @@ package com.github.faridma.simplehttp.core;
 
 import com.github.faridma.simplehttp.HttpMethod;
 import com.github.faridma.simplehttp.util.Extractor;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -14,11 +15,10 @@ import java.net.SocketAddress;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import javax.net.ssl.HttpsURLConnection;
+import javax.xml.bind.DatatypeConverter;
 
 /**
- *
  * @author Farid F. Mammadov
  */
 public class HttpConnection {
@@ -28,7 +28,7 @@ public class HttpConnection {
      * doOutput is true.
      *
      * @param connectionURL endpoint URL string
-     * @param method name of HTTP method [GET/POST]
+     * @param method        name of HTTP method [GET/POST]
      * @return HTTP connection with basic configuration
      * @throws IOException
      */
@@ -47,9 +47,9 @@ public class HttpConnection {
      * doOutput is true.
      *
      * @param connectionURL endpoint URL string
-     * @param method name of HTTP method [GET/POST]
-     * @param proxyHost proxy hostname
-     * @param proxyPort proxy IP address
+     * @param method        name of HTTP method [GET/POST]
+     * @param proxyHost     proxy hostname
+     * @param proxyPort     proxy IP address
      * @return HTTP connection with basic configuration
      * @throws IOException
      */
@@ -66,11 +66,69 @@ public class HttpConnection {
     }
 
     /**
+     * Creates HTTP connection with basic settings. If request method is "POST",
+     * doOutput is true.
+     *
+     * @param url    endpoint URL
+     * @param method name of HTTP method [GET/POST]
+     * @return HTTP connection with basic configuration
+     * @throws IOException
+     */
+    public static HttpURLConnection getConnection(URL url, String method) throws IOException {
+        HttpURLConnection c = (HttpURLConnection) url.openConnection();
+        c.setRequestMethod(method);
+        if (method.equals(HttpMethod.POST)) {
+            c.setDoOutput(true);
+        }
+        return c;
+    }
+
+    /**
+     * Creates HTTP connection with basic settings. If request method is "POST",
+     * doOutput is true.
+     *
+     * @param url       endpoint URL string
+     * @param method    name of HTTP method [GET/POST]
+     * @param proxyHost proxy hostname
+     * @param proxyPort proxy IP address
+     * @return HTTP connection with basic configuration
+     * @throws IOException
+     */
+    public static HttpURLConnection getConnection(URL url, String method, String proxyHost, int proxyPort) throws IOException {
+        SocketAddress proxyAddr = new InetSocketAddress(proxyHost, proxyPort);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, proxyAddr);
+        HttpURLConnection c = (HttpURLConnection) url.openConnection(proxy);
+        c.setRequestMethod(method);
+        if (method.equals(HttpMethod.POST)) {
+            c.setDoOutput(true);
+        }
+        return c;
+    }
+
+    /**
+     * Creates HTTPS connection with basic settings. If request method is
+     * "POST", doOutput is true.
+     *
+     * @param url    endpoint URL
+     * @param method name of HTTP method [GET/POST]
+     * @return HTTPS connection with basic configuration
+     * @throws IOException
+     */
+    public static HttpsURLConnection getSecureConnection(URL url, String method) throws IOException {
+        HttpsURLConnection c = (HttpsURLConnection) url.openConnection();
+        c.setRequestMethod(method);
+        if (method.equals(HttpMethod.POST)) {
+            c.setDoOutput(true);
+        }
+        return c;
+    }
+
+    /**
      * Creates HTTPS connection with basic settings. If request method is
      * "POST", doOutput is true.
      *
      * @param connectionURL endpoint URL string
-     * @param method name of HTTP method [GET/POST]
+     * @param method        name of HTTP method [GET/POST]
      * @return HTTPS connection with basic configuration
      * @throws IOException
      */
@@ -89,9 +147,9 @@ public class HttpConnection {
      * "POST", doOutput is true.
      *
      * @param connectionURL endpoint URL string
-     * @param method name of HTTP method [GET/POST]
-     * @param proxyHost proxy hostname
-     * @param proxyPort proxy IP address
+     * @param method        name of HTTP method [GET/POST]
+     * @param proxyHost     proxy hostname
+     * @param proxyPort     proxy IP address
      * @return HTTPS connection with basic configuration
      * @throws IOException
      */
@@ -108,12 +166,34 @@ public class HttpConnection {
     }
 
     /**
+     * Creates HTTPS connection with basic settings. If request method is
+     * "POST", doOutput is true.
+     *
+     * @param connectionURL endpoint URL
+     * @param method        name of HTTP method [GET/POST]
+     * @param proxyHost     proxy hostname
+     * @param proxyPort     proxy IP address
+     * @return HTTPS connection with basic configuration
+     * @throws IOException
+     */
+    public static HttpsURLConnection getSecureConnection(URL connectionURL, String method, String proxyHost, int proxyPort) throws IOException {
+        SocketAddress proxyAddr = new InetSocketAddress(proxyHost, proxyPort);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, proxyAddr);
+        HttpsURLConnection c = (HttpsURLConnection) connectionURL.openConnection(proxy);
+        c.setRequestMethod(method);
+        if (method.equals(HttpMethod.POST)) {
+            c.setDoOutput(true);
+        }
+        return c;
+    }
+
+    /**
      * Adds request header to given connection. If header already exists, value
      * overrides.
      *
-     * @param c HTTP connection
+     * @param c          HTTP connection
      * @param headerName HTTP request header name
-     * @param value value to set header
+     * @param value      value to set header
      */
     public static void addRequestHeader(HttpURLConnection c, String headerName, String value) {
         c.setRequestProperty(headerName, value);
@@ -122,19 +202,19 @@ public class HttpConnection {
     /**
      * Adds Authorization header to header
      *
-     * @param c HTTP connection
+     * @param c        HTTP connection
      * @param username Basic authorization login
      * @param password Basic authorization password
      */
     public static void addBasicAuthorization(HttpURLConnection c, String username, String password) {
-        String authorizationString = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+        String authorizationString = DatatypeConverter.printBase64Binary((username + ":" + password).getBytes());
         addRequestHeader(c, "Authorization", "Basic " + authorizationString);
     }
 
     /**
      * Sends HTTP POST request to HTTP connection
      *
-     * @param c HTTP connection
+     * @param c       HTTP connection
      * @param content String representation of request body
      * @throws IOException
      */
@@ -148,7 +228,7 @@ public class HttpConnection {
     /**
      * Converts HTTP response to String
      *
-     * @param c HTTP(S) connection
+     * @param c       HTTP(S) connection
      * @param charset Charset value
      * @return String representation of HTTP response body
      * @throws IOException
@@ -156,9 +236,9 @@ public class HttpConnection {
     public static String getResponse(HttpURLConnection c, Charset charset) throws IOException {
         int responseCode = c.getResponseCode();
         StringBuilder response = new StringBuilder();
-        try (InputStream is = ((responseCode >= 200) && (responseCode <= 299)) ? c.getInputStream() : c.getErrorStream();
-                InputStreamReader isr = new InputStreamReader(is, charset);
-                BufferedReader br = new BufferedReader(isr)) {
+        try (InputStream is = c.getInputStream() == null ? c.getErrorStream() : c.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is, charset);
+             BufferedReader br = new BufferedReader(isr)) {
             response.append(Extractor.getStringValue(br));
         }
         c.disconnect();
